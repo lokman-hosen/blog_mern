c1b2b073749c4cbe11c703e1d21d92db50413434
lokman_hosen <lokman.cse.pstu@gmail.com>
2022-09-24 10:20:43 +0600

    first commit

---
+++ api/index.js
@@ -0,0 +1,32 @@
+import express from "express"
+import mongoose from 'mongoose';
+import authRoute from './routes/auth.js'
+
+const app = express()
+
+import dotenv from "dotenv"
+dotenv.config()
+const connect = async () =>{
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log('connected to database')
+    } catch (error) {
+        throw error;
+    }
+}
+
+mongoose.connection.on("disconnected", ()=>{
+     console.log('MongoBd disconnected')
+})
+
+app.get("/", (req, res) =>{
+    res.send("Hello mongoo app")
+})
+
+//middleware
+app.use("/auth", authRoute);
+
+app.listen(8800, () =>{
+    connect()
+    console.log('connected again')
+})
