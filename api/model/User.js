f29f78740201d5a4264a289af0df5bf788b5ee3b
lokman-hosen <lokman@mcc.com.bd>
2022-09-28 18:13:35 +0600

    User schema done

---
+++ api/model/User.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+    {
+        name: {
+          type: String,
+          required: true
+        },
+        user_type: {
+          type: String,
+          required: true,
+          default: 'author', //admin and author
+        },
+        image: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            trim: true,
+            unique: true,
+            required: true
+        },
+        status: {
+            type: Number,
+            default: 1,
+        },
+    },
+    {
+        timestamps: true
+    }
+);
+
+export default mongoose.model("User", userSchema)
