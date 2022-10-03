e3c5f677669eb21ade25d3361f5d55a1a4db88d6
lokman-hosen <lokman@mcc.com.bd>
2022-10-03 11:18:51 +0600

    Check user authenticated by middleware while get user list

--- api/middlewares/checkLogin.js
+++ api/middlewares/checkLogin.js
@@ -2,12 +2,15 @@ import jwt from "jsonwebtoken";
 
 export const checkLogin = async (req, res, next)=>{
   const { authorization} = req.headers;
-  res.status.json(req.headers)
-  // try{
-  //     const token = authorization.$split(' ')[1]
-  //     jwt.verify(token, process.env.JWT_SECRET);
-  //     next()
-  // }catch (error){
-  //     next("Authentication fail")
-  // }
+ // console.log(authorization)
+  //res.status.json(req.headers)
+  //console.log(req.headers.authorization.split(' ')[1]);
+  //res.status.json('Middleware')
+  try{
+      const token = authorization.split(' ')[1]
+      jwt.verify(token, process.env.JWT_SECRET);
+      next()
+  }catch (error){
+      next("Authentication fail")
+  }
 }
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,13 @@
 import express from "express";
 import User from "../model/User.js";
 import {createUser, deleteUser, updateUser, userDetail, userList} from "../controllers/userController.js";
+import {checkLogin} from "../middlewares/checkLogin.js";
 
 const router = express.Router();
+
+// router.get("/middleware", checkLogin,(req, res)=>{
+//     res.status(200).json('Middlleware')
+// })
 //create
 router.post("/", createUser)
 //update
@@ -12,6 +17,6 @@ router.delete("/:id", deleteUser);
 //detail
 router.get("/:id", userDetail);
 //get all
-router.get("/", userList);
+router.get("/", checkLogin, userList);
 
 export default router
