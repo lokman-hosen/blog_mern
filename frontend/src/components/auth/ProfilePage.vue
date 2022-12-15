a8fc5741539d0c6e762c7a3f8e57925ee9e70a55
lokman-hosen <lokman@mcc.com.bd>
2022-12-15 16:11:45 +0600

    Add loader to load post in profile

--- frontend/src/components/auth/ProfilePage.vue
+++ frontend/src/components/auth/ProfilePage.vue
@@ -4,10 +4,11 @@ import {useAuthStore} from "@/stores/auth";
 import {storeToRefs} from "pinia";
 import {API_BASE_URL} from "@/config";
 import PaginationPage from "@/components/PaginationPage";
+import PageLoader from "@/components/PageLoader";
 
 export default {
   name: 'ProfilePage',
-  components: {PaginationPage},
+  components: {PageLoader, PaginationPage},
   setup(){
     let baseUrl= API_BASE_URL;
     const authStore = useAuthStore();
@@ -74,95 +75,109 @@ export default {
 
 <template>
   <div class="section blog-wrap bg-gray">
+
       <div class="container">
-        <div class="row">
-          <div class="col-3">
-            <ul class="nav flex-column nav-pills border" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-              <div>
-                <img src="https://technext.github.io/megakit-2/images/blog/blog-author.jpg" class="card-img-top" alt="...">
-              </div>
-              <li class="nav-link"
-                  :class="{ active: activeTab == 'profile' }"
-                  @click="handleTab('profile')"
-                  id="v-pills-profile-tab" data-toggle="pill" data-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">
-                Profile
-              </li>
+        <div class="card">
+          <h5 class="card-header">DashBoard: Welcome Back {{user.name}} !!</h5>
+          <div class="card-body">
+            <div class="row">
+              <div class="col-3">
+                <ul class="nav flex-column nav-pills border" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                  <div>
+                    <img src="https://technext.github.io/megakit-2/images/blog/blog-author.jpg" class="card-img-top" alt="...">
+                  </div>
+                  <li class="nav-link"
+                      :class="{ active: activeTab == 'profile' }"
+                      @click="handleTab('profile')"
+                      id="v-pills-profile-tab" data-toggle="pill" data-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">
+                    Profile
+                  </li>
 
-              <li class="nav-link"
-                  :class="{ active: activeTab == 'post' }"
-                  @click="handleTab('post')"
-                  id="v-pills-post-tab" data-toggle="pill" data-target="#v-pills-post" type="button" role="tab" aria-controls="v-pills-post" aria-selected="true">Post</li>
-              <button class="btn btn-info" @click="logout">Logout</button>
-            </ul>
-          </div>
-          <div class="col-9">
-            <div class="tab-content" id="v-pills-tabContent">
-              <div class="tab-pane fade"
-                   :class="{ 'show active': activeTab == 'profile' }"
-                   id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
-                <form id="contact-form" class="contact__form" method="post" action="mail.php">
-                  <!-- form message -->
-                  <div class="row">
-                    <div class="col-12">
-                      <div class="alert alert-success contact__msg" style="display: none" role="alert">
-                        Your message was sent successfully.
+                  <li class="nav-link"
+                      :class="{ active: activeTab == 'post' }"
+                      @click="handleTab('post')"
+                      id="v-pills-post-tab" data-toggle="pill" data-target="#v-pills-post" type="button" role="tab" aria-controls="v-pills-post" aria-selected="true">Post</li>
+                  <button class="btn btn-info" @click="logout">Logout</button>
+                </ul>
+              </div>
+              <div class="col-9">
+                <div class="tab-content" id="v-pills-tabContent">
+                  <div class="tab-pane fade"
+                       :class="{ 'show active': activeTab == 'profile' }"
+                       id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+                    <form id="contact-form" class="contact__form" method="post" action="mail.php">
+                      <!-- form message -->
+                      <div class="row">
+                        <div class="col-12">
+                          <div class="alert alert-success contact__msg" style="display: none" role="alert">
+                            Your message was sent successfully.
+                          </div>
+                        </div>
                       </div>
+                      <!-- end message -->
+                      <div class="form-group">
+                        <input name="name" type="text" v-model="name" class="form-control" placeholder="Your Name">
+                      </div>
+                      <div class="form-group">
+                        <input name="email" type="email" v-model="email" class="form-control" placeholder="Email Address">
+                      </div>
+                      <div class="form-group">
+                        <input name="password" type="password" class="form-control" placeholder="Password">
+                      </div>
+
+                      <button class="btn btn-main" name="submit" type="submit">Update Profile</button>
+                    </form>
+                  </div>
+
+                  <div class="tab-pane fade"
+                       :class="{ 'show active': activeTab == 'post' }"
+                       id="v-pills-post" role="tabpanel" aria-labelledby="v-pills-post-tab">
+                    <div class="text-right mb-3">
+                      <button class="btn btn-info" @click="openModal">Create New</button>
                     </div>
-                  </div>
-                  <!-- end message -->
-                  <div class="form-group">
-                    <input name="name" type="text" v-model="name" class="form-control" placeholder="Your Name">
-                  </div>
-                  <div class="form-group">
-                    <input name="email" type="email" v-model="email" class="form-control" placeholder="Email Address">
-                  </div>
-                  <div class="form-group">
-                    <input name="password" type="password" class="form-control" placeholder="Password">
-                  </div>
+                    <div v-if="userPosts.length > 0">
+                      <table class="table table-bordered table-sm">
+                        <thead>
+                        <tr>
+                          <th scope="col">#</th>
+                          <th scope="col">Img</th>
+                          <th scope="col">Title</th>
+                          <th scope="col">Author</th>
+                          <th scope="col">Status</th>
+                          <th scope="col">Published At</th>
+                          <th scope="col">Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr  v-for="(post, index) in userPosts" :key="post._id">
+                          <th scope="row">{{index+1}}</th>
+                          <td><img :src="baseUrl+post.image" alt="" class="rounded" height="50" width="50"></td>
+                          <td>{{post.title}}</td>
+                          <td>{{post.author.name}}</td>
+                          <td>{{post.status == 1 ? 'Published' : 'Pending'}}</td>
+                          <td>{{post.createdAt}}</td>
+                          <td>
+                            <router-link :to="'post/' + post._id">
+                              <i class="fa fa-eye"></i>
+                            </router-link>
+                          </td>
+                        </tr>
+                        </tbody>
+                      </table>
+                      <PaginationPage :totalRecord="totalRecord" @change-page="changePage" :currentPage="currentPage"/>
+                    </div>
+                    <div v-else>
+                      <PageLoader></PageLoader>
+                    </div>
 
-                  <button class="btn btn-main" name="submit" type="submit">Update Profile</button>
-                </form>
-              </div>
 
-              <div class="tab-pane fade"
-                   :class="{ 'show active': activeTab == 'post' }"
-                   id="v-pills-post" role="tabpanel" aria-labelledby="v-pills-post-tab">
-                <div class="text-right mb-3">
-                  <button class="btn btn-info" @click="openModal">Create New</button>
+                  </div>
                 </div>
-                <table class="table table-bordered table-sm">
-                  <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Img</th>
-                    <th scope="col">Title</th>
-                    <th scope="col">Author</th>
-                    <th scope="col">Status</th>
-                    <th scope="col">Published At</th>
-                    <th scope="col">Action</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  <tr v-for="(post, index) in userPosts" :key="post._id">
-                    <th scope="row">{{index+1}}</th>
-                    <td><img :src="baseUrl+post.image" alt="" class="rounded" height="50" width="50"></td>
-                    <td>{{post.title}}</td>
-                    <td>{{post.author.name}}</td>
-                    <td>{{post.status == 1 ? 'Published' : 'Pending'}}</td>
-                    <td>{{post.createdAt}}</td>
-                    <td>
-                      <router-link :to="'post/' + post._id">
-                        <i class="fa fa-eye"></i>
-                      </router-link>
-                    </td>
-                  </tr>
-                  </tbody>
-                </table>
-                <PaginationPage :totalRecord="totalRecord" @change-page="changePage" :currentPage="currentPage"/>
               </div>
             </div>
           </div>
         </div>
+
       </div>
   </div>
 
@@ -176,7 +191,6 @@ export default {
         </div>
         <form @submit.prevent="handlePostSubmit">
         <div class="modal-body">
-
             <div class="row">
               <div class="col-12">
                 <div class="form-group mb-3">
@@ -215,7 +229,6 @@ export default {
               </div>
 
             </div>
-
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" @click="openModal">Close</button>
